010339d53d64afabbbc7e49438c34a967d32818f
Modou Aicha Diop <modouaichadiop@gmail.com>
2023-07-28 17:08:44 +0000

    users.urls.py update

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,4 @@ urlpatterns=[
     path("",views.index,name="index"),
     path("login",views.login_view,name="login"),
     path("logout",views.logout_view,name="logout"),
-    path("logout",views.logout_view,name="logout"),
 ]
